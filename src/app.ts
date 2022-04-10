8e5bf93fe1d86d20b8e55f243e70478f2cdc171e
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-04-10 11:56:58 +0100

    added cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routes';
+import cors from 'cors';
 
 const port = process.env.PORT || 8081;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', router);
 app.get('/', (req, res) => {
   res.json({ success: true });
