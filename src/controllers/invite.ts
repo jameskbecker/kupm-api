7b24bb6d9fbc2ec36e488e01900c3677e71e546d
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-05-21 15:55:30 +0100

    Update invite.ts

--- src/controllers/invite.ts
+++ src/controllers/invite.ts
@@ -8,14 +8,9 @@ import { insertUserProject } from '../db/queries/userProject';
 import { selectUserIdByEmail } from '../db/queries/user';
 
 const defaultBody = { success: false };
-const defaultHeaders = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
-};
 
 const postInvite = async (req: Request, res: Response) => {
   let body: any = { ...defaultBody };
-  res.set(defaultHeaders);
 
   try {
     const dataPacket = await selectUserIdByEmail(req.body.email);
@@ -38,7 +33,6 @@ const postInvite = async (req: Request, res: Response) => {
 
 const postInviteJoin = async (req: Request, res: Response) => {
   let body: any = { ...defaultBody };
-  res.set(defaultHeaders);
 
   try {
     const dataPacket = await selectUserIdByEmail('fdk');
